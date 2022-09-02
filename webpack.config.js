2a6068f133acb652e0e82458bd9d856d02ecadcc
Asael Hernández <asael.dev@gmail.com>
2022-09-02 00:31:56 -0500

    Added scss rules to webpack.config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,10 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
         test: /\.png/,
         type: "asset/resource",
       },
