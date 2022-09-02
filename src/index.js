d93de7b76b3e31b9b7fec72c0db39578de20a261
Asaek Hernández <asael.dev@gmail.com>
2022-09-02 00:00:32 -0500

    1) Added initial UI. 2) Added API_URL to .env.example. 3) Added api.js for handling api calls.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./scss/main.scss";
+import * as bootstrap from "bootstrap";
+
+import { getMealsByFirstLetter, getMealsByName } from "./js/api";
+
+(async () => {
+  let response = await getMealsByName("Arrabiata");
+  console.log(response);
+
+  response = await getMealsByFirstLetter("A");
+  console.log(response);
+})();
